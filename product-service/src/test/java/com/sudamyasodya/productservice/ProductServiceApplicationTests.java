fcfb6b4e82dd20d5a9c13910e99573e66d503b34
gdsghost <sudam19961994@gmail.com>
2023-02-03 09:22:13 +0530

    initial commit

---
+++ product-service/src/test/java/com/sudamyasodya/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudamyasodya.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
