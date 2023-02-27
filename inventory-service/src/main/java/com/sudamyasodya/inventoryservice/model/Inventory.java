375381ad4fc40cd55af7a19cd5975c5a1ff2f001
Sudam Yasodya <sudam19961994@gmail.com>
2023-02-27 19:06:19 +0530

    inventory service completed

---
+++ inventory-service/src/main/java/com/sudamyasodya/inventoryservice/model/Inventory.java
@@ -0,0 +1,22 @@
+package com.sudamyasodya.inventoryservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "t_inventory")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String skuCode;
+    private Integer quantity;
+}
---
+++ inventory-service/src/main/java/com/sudamyasodya/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.sudamyasodya.inventoryservice.repository;
+
+import com.sudamyasodya.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
